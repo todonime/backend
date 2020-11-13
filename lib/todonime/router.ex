712eb07ee01e55ac24dc35603bb93e3abdffc7ba
Alexey Fedorenko <alex@grutenko.ru>
2020-11-13 18:26:35 +0300

    Add default router.

--- lib/todonime/router.ex
+++ lib/todonime/router.ex
@@ -9,6 +9,13 @@ defmodule Todonime.Router do
   get "/watch/:id", do: Todonime.Controller.Watch.get(conn, id)
   post "/auth", do: Todonime.Controller.Auth.internal(conn)
 
+  match "" do
+    {:ok, version} = :application.get_key(:todonime, :vsn)
+    send_resp(conn, 200, "Todonime #{List.to_string(version)}.")
+  end
+
+  get _, do: send_resp(conn, 404, "{\"message\": \"Route not found.\"}")
+
   def handle_errors(conn, %{kind: _kind, reason: reason, stack: _stack}) do
     case reason.__struct__ do
       Todonime.Exception.ClientException -> reason.message
