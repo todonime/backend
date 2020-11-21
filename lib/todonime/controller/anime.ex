7527e39cf2acb493599f6f4125eb7853cfecafc6
Alexey Fedorenko <alex@grutenko.ru>
2020-11-22 01:27:02 +0300

    Code review

--- lib/todonime/controller/anime.ex
+++ lib/todonime/controller/anime.ex
@@ -1,13 +1,17 @@
 defmodule Todonime.Controller.Anime do
   def get(conn, anime_id) do
-    anime = Todonime.Mapper.Anime.get!(anime_id)
-    user = Guardian.Plug.current_resource(conn)
-    rate = (if user != nil, do: Todonime.User.rate_for!(user, anime))
-
     Todonime.Mapper.Anime.get!(anime_id)
     |> Todonime.Anime.with_genres!
-    |> (&(if rate != nil, do: Todonime.Anime.apply_rate(&1, rate), else: &1)).()
+    |> maybe_apply_rate(conn)
     |> Jason.encode!
     |> (&Plug.Conn.send_resp(conn, 200, &1)).()
+  end
+
+  defp maybe_apply_rate(anime, conn) do
+    with user when not is_nil(user) <- Guardian.Plug.current_resource(conn),
+         rate when not is_nil(rate) <- Todonime.User.rate_for!(user, anime),
+    do: Todonime.Anime.apply_rate(anime, rate) 
+        |> (&%{&1 | rate: rate}).(),
+    else: (_ -> anime)
   end
 end
